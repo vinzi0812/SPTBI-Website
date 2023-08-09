5bf7fbe8f6324973369b51f14466a6d8de4bf9e0
Vineet Parmar <vineet.parmar@spit.ac.in>
2023-08-10 01:17:18 +0530

    changes

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from multiselectfield import MultiSelectField
 from django.contrib.auth.models import AbstractUser
 from .manager import UserManager
 from django.utils.text import slugify
